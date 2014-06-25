b1463b41dc55b8ff8ca92b48a6a81482e0826beb
yangml <yangmingliang_22@163.com>
2014-06-25 21:02:41 +0800

    请求映射规则

---
+++ src/cn/bling/chapter6/web/controller/parameter/RequestParameterController2.java
@@ -0,0 +1,22 @@
+package cn.bling.chapter6.web.controller.parameter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/parameter2")                      //①处理器的通用映射前缀
+public class RequestParameterController2 {
+	//②进行类级别的@RequestMapping窄化
+    @RequestMapping(params="submitFlag=create", method=RequestMethod.GET) 
+    public String showForm() {
+        System.out.println("===============showForm");
+        return "parameter/create";        
+    }
+    //③进行类级别的@RequestMapping窄化
+    @RequestMapping(params="submitFlag=create", method=RequestMethod.POST)   
+    public String submit() {
+        System.out.println("===============submit");
+        return "redirect:/success";        
+    }
+}
